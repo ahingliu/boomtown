5b8752728b2a8b5a342d5d2c822c2ea830d4c6d8
hiigara <hiigara@hiigaras-Mac.local>
2019-10-30 10:43:12 -0700

    apollo.js todo list done

--- server/config/apollo.js
+++ server/config/apollo.js
@@ -1,48 +1,36 @@
-const { ApolloServer } = require("apollo-server-express");
-const { makeExecutableSchema } = require("graphql-tools");
+const { ApolloServer } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+const typeDefs = require("../api/schema");
+let resolvers = require("../api/resolvers");
 
-const typeDefs = require("../api/schema");
-let resolvers = require("../api/resolvers");
+module.exports = ({ app, pgResource }) => {
+  resolvers = resolvers(app);
 
-module.exports = ({ app, pgResource }) => {
-  resolvers = resolvers(app);
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
 
-  /**
-   * @TODO: Initialize Apollo Server
-   *
-   * Once you've defined your schema types, it's time to wire up your schema
-   * to your resolving functions. This is Apollo magic, and it's done using
-   * the 'makeExecutableSchema' function provided by the 'graphql-tools' package.
-   *
-   * https://www.apollographql.com/docs/apollo-server/v2/api/graphql-tools.html#makeExecutableSchema
-   */
+  const apolloServer = new ApolloServer({
+    context: ({ req }) => {
+      const tokenName = app.get("JWT_COOKIE_NAME")
+      const token = req ? req.cookies[tokenName] : undefined
+      
+      try {
+        return {
+          req,
+          token,
+          pgResource
+        }
+      } catch (e) {
+        console.log ("this is an error");
+      }
+    },
+    schema,
+  });
 
-  // @TODO: Refactor to use 'makeExecutableSchema' to wire up your schema to your resolvers:
-  const schema = undefined;
-  // -------------------------------
-
-  const apolloServer = new ApolloServer({
-    context: ({ req }) => {
-      // @TODO: Uncomment this later when we add auth (to be added to Apollo's context)
-      // const tokenName = app.get("JWT_COOKIE_NAME")
-      // const token = req ? req.cookies[tokenName] : undefined
-      // let user = null
-      // -------------------------------
-      try {
-        // TODO:
-        // If there is a token, verify that token to get user info and assign it to user variable
-        // return req, token, user, pgResources
-      } catch (e) {
-        // throw error
-      }
-    },
-    schema,
-  });
-
-  apolloServer.applyMiddleware({
-    app,
-    // @TODO: Add the CORS_CONFIG from your application configuration
-    cors: undefined,
-    // -------------------------------
-  });
+  apolloServer.applyMiddleware({
+    app,
+    cors: app.get('CORS_CONFIG'),
+  });
 };
