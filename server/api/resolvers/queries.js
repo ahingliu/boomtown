93e48573ede59a8519ababd7328a0bda32f91ef6
hiigara <hiigara@hiigaras-Mac.local>
2019-10-31 10:27:41 -0700

    queries.js done

--- server/api/resolvers/queries.js
+++ server/api/resolvers/queries.js
@@ -2,32 +2,24 @@ const { ApolloError } = require("apollo-server");
 
 const queryResolvers = app => ({
   viewer(parent, args, { user }, info) {
-    /**
-     * @TODO: Authentication - Server
-     *
-     *  If you're here, you have successfully completed the sign-up and login resolvers
-     *  and have added the JWT from the HTTP cookie to your resolver's context.
-     *
-     *  The viewer is what we're calling the current user signed into your application.
-     *  When the user signed in with their username and password, an JWT was created with
-     *  the user's information cryptographically encoded inside.
-     *
-     *  To provide information about the user's session to the app, return the user.
-     *  If there is no user, the user has signed out, in which case user will be null.
-     */
+    if (user.token) {
+      return jwt.decode(user.toke, app.get('JW_SECRET'));
+    } else
     return null;
   },
   async user(parent, { id }, { pgResource }, info) {
     try {
       const user = await pgResource.getUserById(id);
       user.fullname = user.username;
+      if (id === null) {
+        return null;
+      } else
       return user;
     } catch (e) {
       throw new ApolloError(e);
     }
   },
-
-  async items(parent, { filter }, {pgResource}, info) {
+  async items(parent, { filter }, {pgResource}) {
     try {
       const items = await pgResource.getItems(filter);
       return items;
@@ -35,7 +27,7 @@ const queryResolvers = app => ({
       throw new ApolloError(e);
     }
   },
-  async tags(parent, { }, {pgResource}, info) {
+  async tags(parent, args, {pgResource}, info) {
     try {
       const items = await pgResource.getTags();
       return tags;
